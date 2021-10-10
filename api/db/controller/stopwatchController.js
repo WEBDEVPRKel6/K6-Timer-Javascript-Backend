5fe3a1d456c7972de3040a4971704e04c7b2920e
blekkk <fazailman992@gmail.com>
2021-10-10 10:55:27 +0700

    added add and updated methods

--- api/db/controller/stopwatchController.js
+++ api/db/controller/stopwatchController.js
@@ -11,3 +11,54 @@ exports.getStopwatches = async (req, res) => {
     res.status(500).send(error.message);
   }
 }
+
+exports.addStopwatch = async (req, res) => {
+  const {
+    title,
+    time,
+    date,
+    running
+  } = req.body;
+  
+  try {
+    await Stopwatch.create({
+      title,
+      time,
+      date,
+      running
+    })
+    res.status(201).send('Data created');
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+exports.updateStopwatch = async (req, res) => {
+  const id = req.params.id;
+  const {
+    title,
+    time,
+    date,
+    running
+  } = req.body;
+  
+  try {
+    await Stopwatch.update({
+      title,
+      time,
+      date,
+      running
+    }, {
+      where: {
+        id: id
+      }
+    })
+    res.status(200).send('Data updated');
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+exports.deleteStopwatch = async (req, res) => {
+  console.log('Hello world');
+}
--- api/db/routes/stopwatchRoute.js
+++ api/db/routes/stopwatchRoute.js
@@ -3,4 +3,8 @@ const stopwatch = require('../controller/stopwatchController.js');
 module.exports = (app) => {
   app.route('/stopwatch')
     .get(stopwatch.getStopwatches)
+    .post(stopwatch.addStopwatch)
+
+  app.route('/stopwatch/:id')
+    .put(stopwatch.updateStopwatch)
 }
