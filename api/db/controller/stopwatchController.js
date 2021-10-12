a863608a74c2015a222e25a46e8bb44fa9ad5634
SalmanS <salmanshalahuddin133@gmail.com>
2021-10-12 12:44:34 +0700

    added delete all function and updated routes

--- api/db/controller/stopwatchController.js
+++ api/db/controller/stopwatchController.js
@@ -1,64 +1,78 @@
-const Stopwatch = require('../model/stopwatchModel.js');
+const Stopwatch = require("../model/stopwatchModel.js");
 
 exports.getStopwatches = async (req, res) => {
   try {
     const result = await Stopwatch.findAll();
-    if (!result)
-      res.status(404).send('Data not found');
+    if (!result) res.status(404).send("Data not found");
 
     res.status(200).send(result);
   } catch (error) {
     res.status(500).send(error.message);
   }
-}
+};
 
 exports.addStopwatch = async (req, res) => {
-  const {
-    title,
-    time,
-    date,
-    running
-  } = req.body;
-  
+  const { title, time, date, running } = req.body;
+
   try {
     await Stopwatch.create({
       title,
       time,
       date,
-      running
-    })
-    res.status(201).send('Data created');
+      running,
+    });
+    res.status(201).send("Data created");
   } catch (error) {
     res.status(500).send(error.message);
   }
-}
+};
 
 exports.updateStopwatch = async (req, res) => {
   const id = req.params.id;
-  const {
-    title,
-    time,
-    date,
-    running
-  } = req.body;
-  
+  const { title, time, date, running } = req.body;
+
   try {
-    await Stopwatch.update({
-      title,
-      time,
-      date,
-      running
-    }, {
-      where: {
-        id: id
+    await Stopwatch.update(
+      {
+        title,
+        time,
+        date,
+        running,
+      },
+      {
+        where: {
+          id: id,
+        },
       }
-    })
-    res.status(200).send('Data updated');
+    );
+    res.status(200).send("Data updated");
   } catch (error) {
     res.status(500).send(error.message);
   }
-}
+};
 
 exports.deleteStopwatch = async (req, res) => {
-  console.log('Hello world');
-}
+  const id = req.params.id;
+
+  try {
+    await Stopwatch.destroy({
+      where: {
+        id: id,
+      },
+    });
+    res.status(200).send("Data deleted");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+exports.deleteAllStopwatch = async (req, res) => {
+  try {
+    await Stopwatch.destroy({
+      truncate: true,
+    });
+    res.status(200).send("All data deleted");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
--- api/db/routes/stopwatchRoute.js
+++ api/db/routes/stopwatchRoute.js
@@ -1,10 +1,14 @@
-const stopwatch = require('../controller/stopwatchController.js');
+const stopwatch = require("../controller/stopwatchController.js");
 
 module.exports = (app) => {
-  app.route('/stopwatch')
+  app
+    .route("/stopwatch")
     .get(stopwatch.getStopwatches)
-    .post(stopwatch.addStopwatch)
+    .post(stopwatch.addStopwatch);
 
-  app.route('/stopwatch/:id')
-    .put(stopwatch.updateStopwatch)
-}
+  app.route("/stopwatch/update/:id").put(stopwatch.updateStopwatch);
+
+  app.route("/stopwatch/delete/:id").delete(stopwatch.deleteStopwatch);
+
+  app.route("/stopwatch/delete").delete(stopwatch.deleteAllStopwatch);
+};
