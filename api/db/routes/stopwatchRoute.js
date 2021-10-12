4e7a5505d053ff6e1bbd278944355dbe04cd6407
SalmanS <salmanshalahuddin133@gmail.com>
2021-10-13 01:30:01 +0700

    timer backend v1.0

---
+++ api/db/routes/stopwatchRoute.js
@@ -0,0 +1,14 @@
+const stopwatch = require("../controller/stopwatchController.js");
+
+module.exports = (app) => {
+  app
+    .route("/stopwatch")
+    .get(stopwatch.getStopwatches)
+    .post(stopwatch.addStopwatch);
+
+  app.route("/stopwatch/update/:id").put(stopwatch.updateStopwatch);
+
+  app.route("/stopwatch/delete/:id").delete(stopwatch.deleteStopwatch);
+
+  app.route("/stopwatch/delete").delete(stopwatch.deleteAllStopwatch);
+};
